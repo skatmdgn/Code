93924afc7fd937332ae881d7e82b8f778eea6a5a
skatmdgn <76677821+skatmdgn@users.noreply.github.com>
2021-02-20 15:40:23 +0900

    Add files via upload

---
+++ RPGtest_Code(21.02.20)/UI/ButtonCon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCon : MonoBehaviour
+{
+    private UIManager um;
+
+    [Header("0번 기본창, 1번 가방창, 2번 캐릭터창")]
+    [SerializeField] private GameObject[] obj = null;
+
+    private void Start()
+    {
+        um = FindObjectOfType<UIManager>();
+    }
+
+    public void setAct(bool b)
+    {
+        um.returnSettingBox().SetActive(b);
+        gameObject.SetActive(!b);
+        if (obj[1].activeSelf)
+        {
+            Inven();
+        }
+    }
+
+    public void Inven()
+    {
+        for (int i = 0; i < 2; ++i)
+        {
+            obj[i].SetActive(!obj[i].activeSelf);
+        }
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ RPGtest_Code(21.02.20)/UI/Effect/FadeOut.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour
+{
+    private Image box;
+    private Text txt;
+    private Color boxColor;
+    private Color txtColor;
+
+    [SerializeField] private float timer = 0f;
+    private float timerReset;
+
+    private void Start()
+    {
+        box = GetComponent<Image>();
+        txt = transform.GetChild(0).GetComponent<Text>();
+        boxColor = box.color;
+        txtColor = txt.color;
+        timerReset = timer;
+    }
+
+    private void Update()
+    {
+        if (gameObject.activeSelf == true)
+        {
+            timer -= Time.deltaTime;
+            setColor(timer);
+            if (timer <= 0f)
+            {
+                gameObject.SetActive(false);
+                setColor(1f);
+                timer = timerReset;
+            }
+        }
+    }
+
+    private void setColor(float f)
+    {
+        boxColor.a = f;
+        txtColor.a = f;
+        box.color = boxColor;
+        txt.color = txtColor;
+    }
+}
