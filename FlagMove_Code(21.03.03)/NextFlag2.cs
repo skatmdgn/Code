00f5799fb1681b80b59e88f23793e43e14ec82ea
skatmdgn <76677821+skatmdgn@users.noreply.github.com>
2021-03-03 13:02:27 +0900

    Add files via upload

---
+++ FlagMove_Code(21.03.03)/NextFlag2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextFlag2 : MonoBehaviour
+{
+    [SerializeField] private Transform[] _nextFlag = null;
+    private List<Transform> flagList = new List<Transform>();
+
+    private void Start()
+    {
+        addList();
+    }
+
+    private void addList()
+    {
+        for (int i = 0; i < _nextFlag.Length; ++i)
+        {
+            flagList.Add(_nextFlag[i]);
+        }
+    }
+
+    public Transform returnFlag()
+    {
+        Transform tr = flagList[0];
+        flagList.RemoveAt(0);
+        if (flagList.Count == 0)
+            addList();
+        return tr;
+    }
+}
